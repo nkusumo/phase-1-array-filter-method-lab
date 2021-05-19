b2cc2ac07616835247efdcf0ea38028d202af6e4
nkusumo <nis.kusumo@gmail.com>
2021-05-19 09:56:05 -0700

    commit tree 0fb18ba04e6dafee8f8e3569b96e1673e491672b

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching (driversArray, name) {
+    return driversArray.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch (driversArray, name) {
+    return driversArray.filter(driver => driver.toLowerCase().indexOf(name.toLowerCase()) === 0);
+}
+
+function matchName (driverObjects, nameProp) {
+    return driverObjects.filter(driver => driver.name === nameProp);
+}
